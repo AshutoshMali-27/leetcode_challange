239de0c8509e2eba8d1c109765554a4131d4474c
Ashutosh  Mali <ashutoshmali67@gmail.com>
2023-10-18 22:36:06 +0530

    ispalindrome

---
+++ isPalindrome.cs
@@ -0,0 +1,20 @@
+﻿// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+
+        string str = x.ToString();
+
+
+        char[] charArray = str.ToCharArray();
+
+
+        Array.Reverse(charArray);
+
+        string reversedStr = new string(charArray);
+
+        return str == reversedStr;
+    }
+}
