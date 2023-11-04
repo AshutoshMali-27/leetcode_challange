0eec9a565c23eeb162ce7746c3c4c2c607c77e53
Ashutosh  Mali <ashutoshmali67@gmail.com>
2023-11-04 23:30:41 +0530

    sqrt

---
+++ SQRT.cs
@@ -0,0 +1,43 @@
+﻿Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+
+    public class Solution
+{
+    public int MySqrt(int x)
+    {
+        if (x == 0)
+        {
+            return 0;
+        }
+
+        long left = 1;
+        long right = x;
+        long result = 0;
+
+        while (left <= right)
+        {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+
+            if (square == x)
+            {
+                return (int)mid;
+            }
+            else if (square < x)
+            {
+                left = mid + 1;
+                result = mid; // Update the result while searching for the integer square root.
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return (int)result;
+    }
+}
