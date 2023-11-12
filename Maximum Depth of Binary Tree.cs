321091cb29d0b01097e8ee6255e4d3e19d613319
Ashutosh  Mali <ashutoshmali67@gmail.com>
2023-11-12 23:46:36 +0530

    first commit

---
+++ Maximum Depth of Binary Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+
+
+public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        int leftDepth = MaxDepth(root.left);
+        int rightDepth = MaxDepth(root.right);
+
+        return Math.Max(leftDepth, rightDepth) + 1;
+    }
+}
