8b143d25d34cae6c1c8a561c408632bf01f7abc2
Ashutosh  Mali <ashutoshmali67@gmail.com>
2023-11-17 22:56:00 +0530

    first commit

---
+++ ContainDuplicates.cs
@@ -0,0 +1,42 @@
+﻿//Given an integer array nums, return true if any value appears at least twice in the array,
+//    and return false if every element is distinct.
+
+//Example 1:
+
+//Input: nums = [1, 2, 3, 1]
+//Output: true
+//Example 2:
+
+//Input: nums = [1, 2, 3, 4]
+//Output: false
+//Example 3:
+
+//Input: nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+//Output: true
+
+
+
+
+using System.Collections.Generic;
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+                if (!set.Add(num))
+                {
+                    // If the element is already present in the set, it's a duplicate
+                    return true;
+                }
+            }
+
+            // No duplicates found
+            return false;
+        }
+    }
+}
