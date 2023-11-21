712b7a2ddb424f9fa49a078c1bfd92c6d030a3f1
Ashutosh  Mali <ashutoshmali67@gmail.com>
2023-11-21 22:49:08 +0530

    second commit

--- RemoveDuplicateFromSortedList.cs
+++ RemoveDuplicateFromSortedList.cs
@@ -1,9 +1,9 @@
 ﻿
 
-Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+//Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 
-Input: head = [1, 1, 2]
-Output: [1,2]
+//Input: head = [1, 1, 2]
+//Output: [1,2]
 
 
 public class Solution
