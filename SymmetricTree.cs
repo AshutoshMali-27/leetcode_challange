9c243a7b2c169e0c3f6c9787daeb4340252a04ca
ashutosh <ashutoshmali67@gmail.com>
2023-12-26 22:33:59 +0530

    latest commit

---
+++ SymmetricTree.cs
@@ -0,0 +1,50 @@
+﻿
+//Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+using System;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        if (root == null)
+        {
+            return true;
+        }
+
+        return IsMirror(root.left, root.right);
+    }
+
+    private bool IsMirror(TreeNode left, TreeNode right)
+    {
+        if (left == null && right == null)
+        {
+            return true;
+        }
+
+        if (left == null || right == null)
+        {
+            return false;
+        }
+
+        if (left.val != right.val)
+        {
+            return false;
+        }
+
+        return IsMirror(left.left, right.right) && IsMirror(left.right, right.left);
+    }
+}
+
