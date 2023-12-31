8e9d4dec1a21972c88cc85e31d3aed4e01159387
ashutosh <ashutoshmali67@gmail.com>
2023-12-31 23:40:35 +0530

    latest commit

---
+++ Contains Duplicate II.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections.Generic;
+
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+using System;
+
+public class Class1
+{
+	public Class1()public class Solution
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+
+            Dictionary<int, int> numIndices = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numIndices.ContainsKey(nums[i]) && i - numIndices[nums[i]] <= k)
+                {
+                    return true;
+                }
+
+                numIndices[nums[i]] = i;
+            }
+
+            return false;
+        }
+    }
+	{
+	}
+}
