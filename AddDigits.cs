ac48a6e216c2efa2b5ac9a2e7fc21e23c01967e1
ashutosh <ashutoshmali67@gmail.com>
2024-01-05 23:54:02 +0530

    Add Digit

---
+++ AddDigits.cs
@@ -0,0 +1,26 @@
+﻿//Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+//Input: num = 38
+//Output: 2
+//Explanation: The process is
+//38-- > 3 + 8-- > 11
+//11-- > 1 + 1-- > 2
+//Since 2 has only one digit, return it.
+
+public class Solution
+{
+    public int AddDigits(int num)
+    {
+        while (num >= 10)
+        {
+            int sumDigits = 0;
+            while (num > 0)
+            {
+                sumDigits += num % 10;
+                num /= 10;
+            }
+
+            num = sumDigits;
+        }
+        return num;
+    }
+}
