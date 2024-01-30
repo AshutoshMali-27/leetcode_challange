83c39dcb791ab33fa6fec0deea8e43be5860e2d3
ashutosh <ashutoshmali67@gmail.com>
2024-01-30 23:23:11 +0530

    latest commit

---
+++ ConcationatioofArray.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+public class ConcatenateArrays
+{
+    public static void main(String[] args)
+    {
+        int[] nums = { 1, 2, 3 };
+
+        // Calculate the length of the resulting array
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+
+        // Copy elements from nums to ans at the beginning
+        System.arraycopy(nums, 0, ans, 0, n);
+
+        // Copy elements from nums to ans after the first set
+        System.arraycopy(nums, 0, ans, n, n);
+
+        // Print the result
+        System.out.print("Result: ");
+        for (int num : ans)
+        {
+            System.out.print(num + " ");
+        }
+    }
+}
+using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
